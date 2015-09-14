57f925e3bfc6720c9d38e72c321194eaaa063fa4
WIMIMIW <machongqi@126.com>
2015-09-14 20:55:53 +0800

    add TestAsyc.csproj

---
+++ TestAsyc/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TestAsyc
+{
+    class Program
+    {
+        delegate void FunctionDefine(int idx);
+
+        static void Task(int idx)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Thread ID = " + Thread.CurrentThread.ManagedThreadId+"\tTask"+idx+" = "+i);
+            }            
+        }
+
+        static void Main(string[] args)
+        {
+            FunctionDefine func1 = new FunctionDefine(Task);
+            FunctionDefine func2 = new FunctionDefine(Task); 
+
+            IAsyncResult iar1 = func1.BeginInvoke(1, null, null);
+            IAsyncResult iar2 = func2.BeginInvoke(2, null, null);
+
+            Console.WriteLine("Thread ID = " + Thread.CurrentThread.ManagedThreadId + " Start...");
+
+            func1.EndInvoke(iar1);
+            func2.EndInvoke(iar2);
+
+            Console.WriteLine("Finish!");
+
+            for (; ; ) ;
+        }
+    }
+}
